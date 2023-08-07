28738608c05382eb6c7bacf89caea66fe7d01774
KE.REM <k.avcil@gmail.com>
2023-08-07 23:40:55 +0100

    preattribute change

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -23,6 +23,25 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
 }
 
+void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	
+	Super::PreAttributeChange(Attribute, NewValue);
+	
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+		//Debug change output
+		//UE_LOG(LogTemp, Warning, TEXT("Health: %f"), NewValue);
+	}
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+		//Debug change output
+		//UE_LOG(LogTemp, Warning, TEXT("Mana: %f"), NewValue);
+	}
+}
+
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
 {
 	 GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
