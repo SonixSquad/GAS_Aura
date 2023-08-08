387f1655abdfe78925421f2e3afaaa57f9f541dc
KE.REM <k.avcil@gmail.com>
2023-08-08 21:56:39 +0100

    gameplay effect delegates

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -38,6 +38,12 @@ void AAuraEnemy::UnHighLightActor()
 void AAuraEnemy::BeginPlay()
 {
 	Super::BeginPlay();
+	InitAbilityActorInfo();
+}
+
+void AAuraEnemy::InitAbilityActorInfo()
+{
 	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->AbilityActorInfoSet();
 }
 
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -26,5 +26,6 @@ protected:
 	
 	virtual void BeginPlay() override;
 
+	virtual void InitAbilityActorInfo() override;
 	
 };
