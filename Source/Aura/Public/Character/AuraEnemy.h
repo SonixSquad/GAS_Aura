6b71b9a4bb71821be5142e3d9d91cd3d80935b31
KE.REM <k.avcil@gmail.com>
2023-09-04 09:02:54 +0100

    Player Level & COmbat Interface

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -21,11 +21,20 @@ public:
 	virtual void HighLightActor() override;
 	virtual void UnHighLightActor() override;
 	/** End Enemy Interface */
+
+	/** Combat Interface **/
+
+	virtual int32 GetPlayerLevel() override;
+
+	/** End Combat Interface **/
 	
 protected:
 	
 	virtual void BeginPlay() override;
 
 	virtual void InitAbilityActorInfo() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
+	int32 Level = 1;
 	
 };
--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -18,10 +18,15 @@ UCLASS()
 class AURA_API AAuraPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
+	
 public:
 	AAuraPlayerState();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
+
+	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+	
 protected:
 
 	UPROPERTY(VisibleAnywhere)
@@ -29,5 +34,13 @@ protected:
 
 	UPROPERTY()
 	TObjectPtr<UAttributeSet> AttributeSet;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_Level)
+	int32 Level = 1;
+
+	UFUNCTION()
+	void OnRep_Level(int32 OldLevel);
 	
 };
