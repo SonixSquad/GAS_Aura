8c72c2ec619cbbf4bebfaa2f12eef3d6bca6d2cb
KE.REM <k.avcil@gmail.com>
2023-09-06 19:04:05 +0100

    Attmenu WIdget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Kerem Avcil.
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
