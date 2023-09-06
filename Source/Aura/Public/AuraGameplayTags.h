c226051ce044df3039a85bff1de3ad55e7781b0f
KE.REM <k.avcil@gmail.com>
2023-09-06 06:44:32 +0100

    Gameplay Tags singleton

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,29 @@
+// Copyright Kerem Avcil.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+
+/**
+ *  AuraGameplayTags
+ *
+ *  Singleton containing native gameplay tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+static const FAuraGameplayTags& Get() { return GameplayTags;}
+static void InitializeNativeGameplayTags();
+
+protected:
+
+
+
+private:
+static FAuraGameplayTags GameplayTags;
+
+ 
+};
