2be163927109c2286e9193e8e0209ec6ede007b2
KE.REM <k.avcil@gmail.com>
2023-09-16 16:35:57 +0100

    Aura Projectile Spell template setup

---
+++ Source/Aura/Public/AbilitySystem/GameplayAbilities/AuraProjectileSpell.h
@@ -0,0 +1,20 @@
+// Copyright Kerem Avcil.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/GameplayAbilities/AuraGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjectileSpell : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData);
+};
