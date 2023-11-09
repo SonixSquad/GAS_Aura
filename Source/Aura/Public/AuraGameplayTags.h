101f6885ab65ad0e5fc6ff7cc2f174f35078e421
KE.REM <k.avcil@gmail.com>
2023-11-09 00:02:46 +0000

    18-4 MOntage + socket tags refactor

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -62,9 +62,14 @@ public:
 	//Attack Ability tags
 	FGameplayTag Abilities_Attack; //generic attack tag
 
-	FGameplayTag Montage_Attack_Weapon;
-	FGameplayTag Montage_Attack_RightHand;
-	FGameplayTag Montage_Attack_LeftHand;
+	FGameplayTag CombatSocket_Weapon;
+	FGameplayTag CombatSocket_RightHand;
+	FGameplayTag CombatSocket_LeftHand;
+
+	FGameplayTag Montage_Attack_1;
+	FGameplayTag Montage_Attack_2;
+	FGameplayTag Montage_Attack_3;
+	FGameplayTag Montage_Attack_4;
 
 	TMap<FGameplayTag, FGameplayTag> DamageTypesToResistances;
 	
